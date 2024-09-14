73247b76d2faf2f16680158b0352a4d1c17c4c22
bharathkumar369 <bharathpathapati@gmail.com>
2024-09-14 16:32:56 +0530

    dynamic signin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Slot,Stack,SplashScreen } from 'expo-router'
 import { useFonts } from 'expo-font';
+import GlobalProvider from '../context/GlobalProvider'
 
 SplashScreen.preventAutoHideAsync();
 
@@ -27,12 +28,15 @@ const RootLayout = () => {
   if(!fontsLoaded && !error) return null;
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{headerShown:false}}/>
-      <Stack.Screen name='(auth)' options={{headerShown:false}}/>
-      <Stack.Screen name='(tabs)' options={{headerShown:false}}/>
-      {/* <Stack.Screen name='/search/[query]' options={{headerShown:false}}/> */}
-    </Stack>
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name='index' options={{headerShown:false}}/>
+        <Stack.Screen name='(auth)' options={{headerShown:false}}/>
+        <Stack.Screen name='(tabs)' options={{headerShown:false}}/>
+        {/* <Stack.Screen name='/search/[query]' options={{headerShown:false}}/> */}
+      </Stack>
+    </GlobalProvider>
+
   )
 }
 
--- app/index.jsx
+++ app/index.jsx
@@ -4,8 +4,14 @@ import { Redirect,router } from "expo-router"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { images } from "../constants"
 import CustomButton from "../components/CustomButton"
+import { useGlobalContext } from "../context/GlobalProvider"
 
 const App = () => {
+
+  const {isLoading,isLoggedIn} = useGlobalContext();
+
+  if(!isLoading && isLoggedIn) return <Redirect href="/Home"/>
+
   return(
     // <View className="flex flex-1 px-2 items-center justify-center bg-white">
     //   <Text className="text-3xl font-pblack">Hello</Text>
